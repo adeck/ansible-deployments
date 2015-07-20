d38f3730a37dc6b1f8e9eeb3f0f6c799451a9c0f
devel <devel@jesssie-test-64>
2015-07-20 05:15:00 -0400

    fixed bug in lib.sh, added common roles support

--- lib.sh
+++ lib.sh
@@ -3,24 +3,38 @@
 # A library to make starting on new projects easier.
 #
 
+COMMON_ROLES=(ssh ferm fwknopd)
 DEPLOY_HOME='../../shared'
 
-# Creates the symbolic links and directories all projects need
+# creates the symbolic links and directories all projects need
 create() {
+  # create the role's directory if name specified; otherwise assume we're in it
   [ $# -eq 1 ] && {
     mkdir "$1"
     cd "$1"
   }
+  # create and link resources all projects need
   mkdir plays roles hosts
   _link host_vars
   _link group_vars
   _link ansible.cfg
+  # ...including roles common to all projects.
+  for role in ${COMMON_ROLES[@]} ; do
+    lnrole "$role"
+  done
 }
 
 # Used to link a given ansible inventory file to the current project
-link_host() {
+lnhost() {
   local dir="$DEPLOY_HOME"
-  ln -s "$dir/hosts/$1" "hosts/$1"
+  local file="hosts/$1"
+  ln -s "../$dir/$file" "$file"
+}
+
+lnrole() {
+  local dir="$DEPLOY_HOME"
+  local file="roles/$1"
+  ln -s "../$dir/$file" "$file"
 }
 
 #### Helper functions
